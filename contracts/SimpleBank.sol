a335ae9780092b53d2bdc5c906e20076737145ef
ArcadioGM <arcadiogm.com@gmail.com>
2021-11-03 22:41:20 +0100

    fallback() comment

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -39,9 +39,10 @@ contract SimpleBank {
     // Fallback function - Called if other functions don't match call or sent ether without data
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails otherwise, the sender's money is transferred to contract
-    fallback () external payable {
-        revert();
-    }
+    
+    //fallback () external payable {
+    //    revert();
+    //}
     
     receive () external payable {
         reverts();
